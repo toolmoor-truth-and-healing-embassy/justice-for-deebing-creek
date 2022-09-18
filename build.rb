c49f18f90876de1edba23038214b766929786d45
Rene Wooller <rene@fresho.com>
2022-09-18 14:34:50 +1000

    adds build

---
+++ build.rb
@@ -0,0 +1,23 @@
+require 'yaml'
+gallery_path = "assets/img/gallery"
+
+`mogrify  -format gif -path #{gallery_path}/thumbs -thumbnail 100x100 ./#{gallery_path}/*.*`
+
+files = Dir[gallery_path+"/*"].map do |f| 
+    f.match(/^.*\/(.*\..*)$/) 
+end.compact!.map do |m| 
+    m[1] 
+end
+
+File.open("./_data/gallery.yml", "w") do |f| 
+    f.write(
+        files.map do |fname| 
+            {
+                file: fname,
+                thumb: fname.split(".")[0] + ".gif", 
+                text: fname.split(".")[0],
+            }
+        end.to_yaml
+    )
+end
+
